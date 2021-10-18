703992de158d321aa15081c955fac3f8d0183ad6
Alfredo Garijo <agarijo921@alumnos.iua.edu.ar>
2021-10-18 11:24:46 -0300

    Cambio en diseño

--- app/src/main/java/com/example/museo/MuseoFragment.kt
+++ app/src/main/java/com/example/museo/MuseoFragment.kt
@@ -5,18 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import com.example.museo.databinding.FragmentMuseoBinding
+import com.example.museo.databinding.ItemMuseoBinding
 
 
 class MuseoFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentMuseoBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_museo, container, false)
-        return view
+        binding = FragmentMuseoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if(arguments != null){
+            val usuario = requireArguments().getString("usuario")
+            val et: TextView = binding.bienvenida
+            et.setText("Bienvenido/a " + usuario)
+        }
     }
 
 
